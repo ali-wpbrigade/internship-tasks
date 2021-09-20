6eff213ec2c2c0bc9fd4f18b418547afc8380768
zawwar mughal <ali01.wpbrigade@gmail.com>
2021-09-20 14:05:59 +0500

    mod:errors removed

--- index.php
+++ index.php
@@ -21,204 +21,165 @@ $lname = "";
 $email = "";
 $gender = "";
 
-if(isset($_POST['btn_submit']) ) {
+if( isset($_POST['btn_submit']) ) {
 
-    //firstname
+	if( empty( $_REQUEST['name'] ) ) {
 
-    if( empty( $_REQUEST['name'] ) ) {
-    	$name_err=" First Name Field is Required";
-	} else {
-    $name=check_validity( $_REQUEST['name'] );
-  
+		$name_err=" First Name Field is Required";
+		}else{
+		$name=check_validity( $_REQUEST['name'] );
+		if( !preg_match( "/^[a-zA-Z-' ]*$/",$name ) ){
 
-    if ( !preg_match( "/^[a-zA-Z-' ]*$/",$name ) )  
-    {
-		$name_err=" FIRST NAME FIELD IS not valid";
-		$err = true;
-    }
-
-    if ( strlen($name)<2 || strlen($name)>20 ) 
-    {
-	$nameerr="length is not valid please choose min:2,max:20";
-	$err = true;
+			$name_err = " First Name Field is Not  Valid";
+			$err = true;
+		}
+		if( strlen($name)<2 || strlen($name)>20 ) {
+			$nameerr="Length is not valid please choose min:2,max:20";
+			$err = true;
+		}
 
 	}
+
+
+	//LASTNAME:
+	if( empty( $_REQUEST['lname'] ) ) {
+	$lname_err=" Last Name Field is Required";
+	$err = true;
+	}else{
+		$lname=check_validity( $_REQUEST['lname'] );
+		
+		
+    	if ( !preg_match("/^[a-zA-Z-' ]*$/",$lname) ) {
+		$lname_err=" last name Field iS Not Valid";
+		$err = true;
+		}
+
+		if( strlen( $lname )<2 || strlen( $lname ) >20 ) {
+			$lname_err="length is not valid please choose min:2,max:20";
+			$err = true;
+		}
 	
+	}
+
+
+	//email:
+	if( empty( $_REQUEST['email'] )  ) {
+		$email_err=" Email Field is Required";
+		$err = true;
+	}else {
+		$email=check_validity( $_REQUEST['email'] );
+		if(!filter_var( $email,FILTER_VALIDATE_EMAIL ) ) {
+		$email_err=" Email Field iS Not Valid";
+		$err = true;
+		}
+
+		if ( strlen( $email)<2 || strlen( $email)>20 ) {
+		$email_err="length is not valid please choose min:2,max:20";
+		$err = true;
+		
+		}
+	}
+
+	//address:
+	if ( empty( $_REQUEST['address'] ) ) {
+		$address_err=" Address Field is Required";
+		$err = true;
+	}else {
+		$address=check_validity( $_REQUEST['address'] );
+		if ( strlen( $address)<10 || strlen($address)>50 ) {
+			$address_err="Length is Not Valid please Choose min:10,max:50";
+			$err = true;
+		}
+	}
+
+
+	//phone:
+	if ( empty( $_REQUEST['phone'] ) ) {
+
+		$phone_err=" phone No. Field is Required";
+	 	$err = true;
+	}else {
+		$phone=check_validity( $_REQUEST['phone'] );
+		if ( !preg_match( "/^[0-9 ]*$/",$phone ) ) {
+			$phone_err=" phone Field iS Not Valid";
+			$err = true;
+			
+		}
+		if ( strlen( $phone )<7 || strlen( $phone)>7 ) {
+			$phone_err="length is notvalid plese choose 7 digits";
+			$err = true;
+		}
 		
 
-    
-}
-
-//LASTNAME:
-
-if( empty( $_REQUEST['lname'] ) )
-{
-$lname_err=" NAME FIELD IS REQUIRED";
-$err = true;
-}
-else
-{
-    $lname=check_validity( $_REQUEST['lname'] );
-    filter_var($lname,FILTER_SANITIZE_STRING);
-    if ( !preg_match("/^[a-zA-Z-' ]*$/",$lname) ) {
-	$lname_err=" last name FIELD IS not valid";
-	$err = true;
 	}
+
+	//Actual password:
+	if (empty( $_REQUEST['actualPassword'] )) {
+		$password_err=" password Field is Required";
+		$err = true;
+	}else{
+		$Password=check_validity( $_REQUEST['actualPassword'] );
+		if( strlen( $Password)<6 || strlen( $Password)>2 ){
+			$password_err="length is not valid plese choose min:6,max:20 ";
+			$err = true;
+		}
+
+	}
+
+
+	//Repeat password:
+	if ( empty($_REQUEST['RepeatPassword'] ) ) {
+		$Repeat_Password_err=" Repeat password Field is Required";
+		$err = true;
+
+	}else{
+
+		$RepeatPassword=check_validity( $_REQUEST['RepeatPassword'] );
+		if ( $RepeatPassword!= $_REQUEST['actualPassword'] ) {
+			$Repeat_Password_err="password does not match please try again";
+			$err = true;
+		}
+
+	}
+
+	//gender:
+	if ( empty( $_REQUEST['gender'] ) ) {
+		$gender_err="please select your gender";
+		$err = true;
+	}else{
+		$gender=check_validity( $_REQUEST['gender'] );
+	}
+
+	//agree terms:
+	if (empty( $_REQUEST['agreeterms'] )) {
 	
-    if ( strlen( $lname )<2 || strlen( $lname ) >20 ){
-    // code...
-      $lname_err="length is not valid please choose min:2,max:20";
-	  $err = true;
-    }
-                
-
-}
-
-//email:
-if( empty( $_REQUEST['email'] )  )
-    {
-
-    $email_err=" email FIELD IS REQUIRED";
-	$err = true;
-    }
-    else{
-    $email=check_validity( $_REQUEST['email'] );
-
-    if(!filter_var( $email,FILTER_VALIDATE_EMAIL ) )
-    {
-
-	$email_err=" email FIELD IS not valid";
-	$err = true;
-    }  
-
-    if (strlen( $email)<2 || strlen( $email)>20) 
-    {
-    // code...
-	$email_err="length is notvalid plese choose min:2,max:20";
-	$err = true;
-    }
-
-}   
-
-
-
-//address:  
-if( empty( $_REQUEST['address'] ) ){
-	$address_err=" address FIELD IS REQUIRED";
-	$err = true;
-
-}
-else{
-	$address=check_validity( $_REQUEST['address'] );
- 	if (strlen( $address)<10 || strlen($address)>50) 
-	{
-    // code...
-	$address_err="Length is Not Valid please Choose min:10,max:50";
-	$err = true;
-	}
-
-}
-
-
-
-//phone:
-if( empty( $_REQUEST['phone'] ) ) {
-	 $phone_err=" phone No. FIELD IS REQUIRED";
-	 $err = true;
-}
-
-else{
-    $phone=check_validity( $_REQUEST['phone'] );
-	if ( !preg_match( "/^[0-9 ]*$/",$phone) ) {
-	$phone_err=" phone FIELD IS not valid";
-	$err = true;
-}
-
-if ( strlen( $phone )<7 || strlen( $phone)>7 )  {
-    // code...
-	$phone_err="length is notvalid plese choose 7 digits";
-	$err = true;
-
-     }
-
-}
-
-//Actual password: 
-if( empty( $_REQUEST['actualPassword'] ) ) {
-$password_err=" password FIELD IS REQUIRED";
-$err = true;
-}
-
-else{
-
-	$Password=check_validity( $_REQUEST['actualPassword'] );
-
-	if ( strlen( $Password)<6 || strlen( $Password)>20 ) {
-    // code...
-	$password_err="length is notvalid plese choose min:6,max:20 ";
-	$err = true;
-	}
-
-}
-
-//Repeat password:
-
-if( empty($_REQUEST['RepeatPassword'] ) ) {
-
-	$Repeat_Password_err=" Repeat password FIELD IS REQUIRED";
-	$err = true;
-
-}
-
-else{
-	$RepeatPassword=check_validity( $_REQUEST['RepeatPassword'] );
-  
-	if ( $RepeatPassword!= $_REQUEST['actualPassword'] ) {
-	// code...
-	$Repeat_Password_err="password doed not match please try again";
-	$err = true;
+		$agr_err="please accept terms first check the box";
+		$err = true;
 
 	}
-   
-}
+	// Collect Response if, Not any Error Occour
+	if(  ! $err ) {
 
-//gender:
-if ( empty( $_REQUEST['gender'] ) ) {
-
-	$gender_err="please select your gender";   
-	$err = true;
-}
-else{
-	$gender=check_validity( $_REQUEST['gender'] );
-}
+		echo"<h1>YOUR RECORD SUBMITTED SUCCESSFULLY</h1>";
+	}
 
 
 
-//agree terms:
-if ( empty( $_REQUEST['agreeterms'] ) ) {
-// code...
-$agr_err="please accept terms first check the box";
-$err = true;
-}
 
-if( $err === false )   {
-    echo"<h1>YOUR RECORD SUBMITTED SUCCESSFULLY</h1>"; 
+
 
 }
 
-}
+	//function:
+	function check_validity( $data ) {
+		$data=trim( $data );
+		$data=stripslashes( $data );
+		$data=htmlspecialchars( $data );
+		return $data;
 
-
-function check_validity( $data ) {
-$data=trim( $data );
-$data=stripslashes( $data );
-$data=htmlspecialchars( $data );
-return $data;
-
-}
-
-
+	}
 ?>
+
 
 
 <!DOCTYPE html>
